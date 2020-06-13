2f5f358d53266616107a647c0a10566862e615f4
Georgiy Slobodenyuk <georgiy@absoluteweb.com>
2020-06-13 16:50:57 -0700

    chore(syntax): Add missing colors and enable "prettier"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,50 +1,49 @@
 module.exports = {
   purge: {
     enabled: false,
-    content: [
-      'src/**/*.js'
-    ]
+    content: ["src/**/*.js"],
   },
   theme: {
     container: {
-      center: true
+      center: true,
     },
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1440px',
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
     },
     fontFamily: {
-      display: ['Merriweather', 'sans-serif'],
-      body: ['Source Sans Pro', 'sans-serif'],
-      mono: ['Roboto Mono', 'monospace']
+      display: ["Merriweather", "sans-serif"],
+      body: ["Source Sans Pro", "sans-serif"],
+      mono: ["Roboto Mono", "monospace"],
     },
     fontSize: {
-      sm: ['14px', '20px'],
-      base: ['18px', '24px'],
-      md: ['20px', '32px'],
-      lg: ['26px', '34px']
+      sm: ["14px", "20px"],
+      base: ["18px", "24px"],
+      md: ["20px", "32px"],
+      lg: ["26px", "34px"],
     },
     borderWidth: {
-      default: '2px',
-      '0': '0',
-      '1': '1px',
+      default: "2px",
+      "0": "0",
+      "1": "1px",
       // '2': '2px',
       // '4': '4px',
     },
     extend: {
       height: {
-        banner: '680px',
-        menu: '20em'
+        banner: "680px",
+        menu: "20em",
       },
       colors: {
+        bl: {
+          default: "#004185",
+          highlight: "#1F5B99",
+        },
       },
-      spacing: {
-      }
-    }
+      spacing: {},
+    },
   },
-  plugins: [
-    require('@tailwindcss/ui'),
-  ]
+  plugins: [require("@tailwindcss/ui")],
 }
