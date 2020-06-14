daa538dd910f91717b79edf236d1ac9520b3fe50
Georgiy Slobodenyuk <georgiy@absoluteweb.com>
2020-06-14 14:43:23 -0700

    feat(header): Properly slice search

--- src/components/molecules/header.js
+++ src/components/molecules/header.js
@@ -2,6 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React, { useState } from "react"
 import Navigation from "./navigation"
+import Search from "./search"
 import { useStaticImage } from "../../hooks/use-static-image"
 import Img from "gatsby-image"
 import Transition from "../transition"
@@ -250,32 +251,7 @@ const Header = ({ siteTitle }) => {
               <img src={logo.src} alt={"U.S. Department of Education Logo"} />
             </Link>
             <div className="w-2/3 flex-1 flex items-center justify-center lg:ml-6 lg:justify-end">
-              <div className="max-w-lg w-full lg:max-w-xs">
-                <label htmlFor="search" className="sr-only">
-                  What are you searching for...
-                </label>
-                <div className="relative">
-                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                    <svg
-                      className="h-5 w-5 text-gray-400"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </div>
-                  <input
-                    id="search"
-                    className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:border-blue-300 focus:shadow-outline-blue sm:text-sm transition duration-150 ease-in-out"
-                    placeholder="What are you searching for..."
-                    type="search"
-                  />
-                </div>
-              </div>
+              <Search />
             </div>
           </div>
         </div>
---
+++ src/components/molecules/search.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react"
+
+// TODO: https://www.npmjs.com/package/react-click-outside
+const Search = () => {
+  return (
+    <div className="max-w-lg w-full lg:max-w-xs">
+      <label htmlFor="search" className="sr-only">
+        What are you searching for...
+      </label>
+      <div className="relative">
+        <input
+          id="search"
+          className="block w-full pr-10 pl-3 py-2 border-1 border-bl rounded leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:border-blue-300 focus:shadow-outline-blue sm:text-sm transition duration-150 ease-in-out"
+          placeholder="What are you searching for..."
+          type="search"
+        />
+        <div className="bg-bl absolute inset-y-0 right-0 px-4 flex items-center pointer-events-none rounded-r-md">
+          <svg
+            className="h-5 w-5 text-white"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Search
