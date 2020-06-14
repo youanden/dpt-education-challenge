7bd388221e1f8672a2fc132e9b1650dd13475688
Georgiy Slobodenyuk <georgiy@absoluteweb.com>
2020-06-13 21:25:46 -0700

    feat(layout): Make sure 1680 is the XL breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
       sm: "640px",
       md: "768px",
       lg: "1024px",
-      xl: "1440px",
+      xl: "1680px",
     },
     fontFamily: {
       display: ["Merriweather", "sans-serif"],
